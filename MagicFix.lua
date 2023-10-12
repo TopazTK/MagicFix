71130f36709fc98d924f5543a67b804d3c7446f6
Topaz Whitelock <topaztk@yahoo.com>
2023-10-12 23:46:16 +0300

    Try to fix Magic Levels.

--- MagicFix.lua
+++ MagicFix.lua
@@ -7,7 +7,6 @@ MagicSecond = 0x446131
 
 MagicOld = 0x00
 MagicCount = 0x00
-MagicApplied = 0
 
 function _OnFrame()
     if (EXE_ADDRESS & 0xF0000000) == 0xF0000000 then
@@ -17,45 +16,46 @@ function _OnFrame()
     MagicRead = ReadInt(MagicFirst) + ReadShort(MagicSecond) 
     if MagicOld < MagicRead then
         MagicCount = 0
-        tableMagic = {0x31, 0x32, 0x33, 0x34, 0xAE, 0xB1}
+        tableMagic = {0x31, 0x33, 0x32, 0x34, 0xAE, 0xB1}
         for i = 0, 5 do
             _magicPoint = CallReturn(0x3C3240, i)
 
             if _magicPoint ~= 0x00 then     
-            _sizeRead = CallReturn(0x39E2F0, MEMORY_OFFSET + _magicPoint + 0x04)  
-            _allocMemory = CallReturn(0x150030, _sizeRead)  
+                _sizeRead = CallReturn(0x39E2F0, MEMORY_OFFSET + _magicPoint + 0x04)  
+                _allocMemory = CallReturn(0x150030, _sizeRead)  
 
-            JumpFunction(0x39E4E0, MEMORY_OFFSET + _magicPoint + 0x04, MEMORY_OFFSET + _allocMemory)
+                JumpFunction(0x39E4E0, MEMORY_OFFSET + _magicPoint + 0x04, MEMORY_OFFSET + _allocMemory)
 
-            BAR_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x08, true)
+                BAR_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x08, true)
 
-            PAX_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x18, true) - BAR_OFFSET
-            MAG_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x28, true) - BAR_OFFSET
+                PAX_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x18, true) - BAR_OFFSET
+                MAG_OFFSET = ReadInt(MEMORY_OFFSET + _allocMemory + 0x28, true) - BAR_OFFSET
 
-            WriteLong(0x24BCC7A + (0x50 * i), MEMORY_OFFSET + _magicPoint) 
-            WriteLong(0x24BCC32 + (0x50 * i), MEMORY_OFFSET + _allocMemory)
-            WriteLong(0x24BCC3A + (0x50 * i), MEMORY_OFFSET + _allocMemory + MAG_OFFSET)
-            WriteLong(0x24BCC42 + (0x50 * i), MEMORY_OFFSET + _allocMemory + MAG_OFFSET)
-            WriteLong(0x24BCC52 + (0x50 * i), MEMORY_OFFSET + _allocMemory + PAX_OFFSET + 0x10)
+                WriteLong(0x24BCC7A + (0x50 * i), MEMORY_OFFSET + _magicPoint) 
+                WriteLong(0x24BCC32 + (0x50 * i), MEMORY_OFFSET + _allocMemory)
+                WriteLong(0x24BCC3A + (0x50 * i), MEMORY_OFFSET + _allocMemory + MAG_OFFSET)
+                WriteLong(0x24BCC42 + (0x50 * i), MEMORY_OFFSET + _allocMemory + MAG_OFFSET)
+                WriteLong(0x24BCC52 + (0x50 * i), MEMORY_OFFSET + _allocMemory + PAX_OFFSET + 0x10)
 
-            CallFunction(0x2C3D80, EXE_ADDRESS + 0x2A21198 + (0x50 * i), MEMORY_OFFSET + _allocMemory + PAX_OFFSET)
+                CallFunction(0x2C3D80, EXE_ADDRESS + 0x2A21198 + (0x50 * i), MEMORY_OFFSET + _allocMemory + PAX_OFFSET)
 
-            WriteByte(0x24AA33A + (0x02 * MagicCount), tableMagic[i + 1])
-            MagicCount = MagicCount + 1
+                for z = 0, 5 do
+                    MAGIC_COMMAND = ReadShort(0x24AA33A + (0x02 * z))
+                    
+                    if MAGIC_COMMAND == tableMagic[i + 1] then
+                        break
+                    elseif MAGIC_COMMAND == 0x00 then
+                        WriteByte(0x24AA33A + (0x02 * z), tableMagic[i + 1])
+                        break
+                    end
+                end
+                
+                MagicCount = MagicCount + 1
             end
         end
         
         WriteArray(EXE_ADDRESS + 0x3C314A, {0x90, 0x90, 0x90, 0x90, 0x90}, true)
 
         MagicOld = MagicRead
-        MagicApplied = 1
-    end
-
-    if MagicApplied == 1 and ReadByte(0x0453B82) == 0x00 then
-        MagicApplied = 2
-    end
-
-    if MagicApplied == 2 and ReadByte(0x0453B82) == 0x01 then
-        WriteArray(EXE_ADDRESS + 0x3C314A, {0xE8, 0x01, 0x6F, 0xFD, 0xFF}, true)
     end
 end
